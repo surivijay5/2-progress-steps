2d64d9d7bb1a704a6222bd51a1847c890321633b
Vijay Sai <surivijay5@gmail.com>
2021-09-15 11:22:01 +0530

    Add stp functionality

--- script.js
+++ script.js
@@ -3,19 +3,51 @@ const nextBtn = document.querySelector('#next')
 const progress = document.querySelector('#progress')
 const circles = document.querySelectorAll('.circle')
 
-function getCurrSelectedCircle(){
-    return progress.dataset.id
-}
-prevBtn.addEventListener('click',e => {
+let curSelectedValue = 1
 
+prevBtn.addEventListener('click',e => {
+    curSelectedValue--
+
+    if(curSelectedValue < 1 ) curSelectedValue = 1
+
+    update()
 })
 
 nextBtn.addEventListener('click',e => {
-    let curVal = parseInt(getCurrSelectedCircle())
-    curVal ++
-    if(curVal <= 4){
-        circles[curVal - 1].classList.add('active')
-        progress.style.width 
+    curSelectedValue++
+
+    if(curSelectedValue > circles.length) curSelectedValue = circles.length
+
+    update()
+})
+
+function update(){
+    disableBtns()
+    circles.forEach((circle,idx) => {
+        if(idx < curSelectedValue){
+            circle.classList.add('active')
+        }
+        else{
+            circle.classList.remove('active')
+        }
+    })
+
+    progress.style.width = `${((curSelectedValue - 1) / (circles.length - 1)) * 100}%`
+}
+
+function disableBtns(){
+    if(curSelectedValue > 0){
+        prevBtn.disabled = false
+    }
+    else{
+        prevBtn.disabled = true
     }
 
-})
+    if(curSelectedValue >= circles.length){
+        nextBtn.disabled = true
+    }
+    else{
+        nextBtn.disabled = false
+    }
+}
+
